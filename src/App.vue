c8c07abebccd99323e3f5557cfae1699b6094d76
GabrielHCustodio <93563385+GabrielHCustodio@users.noreply.github.com>
2022-04-14 21:36:19 -0300

    This is my commit learning how to use the CSS Scoped.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
+  <HeaderApp />
   <PrimeiroComponente />
   <InfoPessoa />
 </template>
 
 <script>
+  import HeaderApp from './components/Header.vue'
   import PrimeiroComponente from './components/PrimeiroComponent.vue'
   import InfoPessoa from './components/Pessoa.vue'
 
   export default {
     name: 'App',
     components: {
+      HeaderApp,
       PrimeiroComponente,
       InfoPessoa
     }
---
+++ src/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(l, i) in lista" :key="i"><a href="">{{l.nome}}</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderApp",
+        data() {
+            return {
+                lista: [
+                    {nome: 'Home'},
+                    {nome: 'Projetos'},
+                    {nome: 'Serviços'},
+                    {nome: 'Contato'}
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        width: 100%;
+        height: 80px;
+        background-color: black;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+    }
+
+    div ul {
+        color: white;
+        display: flex;
+        list-style: none;
+    }
+
+    div ul li {
+        margin-right: 50px;
+    }
+
+    div ul li a {
+        text-decoration: none;
+        color: white;
+        background-color: rgb(71, 71, 194);
+        padding: 10px;
+        border-radius: 30px;
+        transition: 1s;
+    }
+
+    div ul li a:hover {
+        background-color: rgb(41, 41, 148);
+    }
+</style>
